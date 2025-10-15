f32b8cd50f9f4056a8b30d698db5d7dedab8c726
Lisa <78325361+lisaantal@users.noreply.github.com>
2025-10-15 18:34:49 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/awshelper/main.go
@@ -0,0 +1,23 @@
+// cmd/awshelper/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "awshelper/internal/awshelper"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := awshelper.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
